0bd5f23dff56c2401a878ecedf0f63143bb402e9
yannled <yannlederrey@gmail.com>
2017-11-28 15:58:19 +0100

    ajout fichier java

---
+++ Etudiant.java
@@ -0,0 +1,74 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Etudiant {
+
+   /**
+    * Default constructor
+    */
+   public Etudiant() {
+   }
+
+   /**
+    * 
+    */
+   private String nom;
+
+
+   /**
+    * 
+    */
+   private Groupe groupe;
+
+   /**
+    * @return
+    */
+   public String toString() {
+      // TODO implement here
+      return "";
+   }
+
+   /**
+    * @param nom 
+    * @param groupe
+    */
+   public void Etudiant(String nom, Groupe groupe) {
+      // TODO implement here
+   }
+
+   /**
+    * @param nom
+    */
+   public void Etudiant(String nom) {
+      // TODO implement here
+   }
+
+   /**
+    * @return
+    */
+   public String getNom() {
+      // TODO implement here
+      return "";
+   }
+
+   /**
+    * @return
+    */
+   public Groupe getGroupe() {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param groupe 
+    * @return
+    */
+   public void setGroupe(Groupe groupe) {
+      // TODO implement here
+      return null;
+   }
+
+}
---
+++ Groupe.java
@@ -0,0 +1,131 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Groupe {
+
+   /**
+    * Default constructor
+    */
+   public Groupe() {
+   }
+
+   /**
+    * 
+    */
+   private int no;
+
+
+   /**
+    * 
+    */
+   private Set<Etudiant> etudiants;
+
+   /**
+    * @return
+    */
+   public String toString() {
+      // TODO implement here
+      return "";
+   }
+
+   /**
+    * @param no 
+    * @param etudiants
+    */
+   public void Groupe(int no, LinkedList<Etudiant> etudiants) {
+      // TODO implement here
+   }
+
+   /**
+    * @param no
+    */
+   public void Groupe(int no) {
+      // TODO implement here
+   }
+
+   /**
+    * @return
+    */
+   public LinkedList<Etudiant> getEtudiants() {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @return
+    */
+   public int getNo() {
+      // TODO implement here
+      return 0;
+   }
+
+   /**
+    * @param etudiants 
+    * @return
+    */
+   public void ajouterEtudiants(Etudiant... etudiants) {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param etudiants 
+    * @return
+    */
+   public void supprimerEtudiants(Etudiant... etudiants) {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @return
+    */
+   public void viderGroupe() {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param groupe 
+    * @param etudiants 
+    * @return
+    */
+   public void transfererEtudiants(Groupe groupe, Etudiant... etudiants) {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param groupeA 
+    * @param groupeB 
+    * @param etudiants 
+    * @return
+    */
+   public static void transfererEtudiants(Groupe groupeA, Groupe groupeB, Etudiant... etudiants) {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param groupe 
+    * @return
+    */
+   public void transfererTous(Groupe groupe) {
+      // TODO implement here
+      return null;
+   }
+
+   /**
+    * @param groupeA 
+    * @param groupeB 
+    * @return
+    */
+   public static void transfererTous(Groupe groupeA, Groupe groupeB) {
+      // TODO implement here
+      return null;
+   }
+
+}
