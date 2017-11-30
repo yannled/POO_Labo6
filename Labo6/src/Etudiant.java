80dd0652abb16375e1389fbfaa2186c4f259c853
Ludwig9392 <loic.frueh@gmail.com>
2017-11-30 11:13:36 +0100

    Etudiant fait

--- Labo6/src/Etudiant.java
+++ Labo6/src/Etudiant.java
@@ -33,7 +33,12 @@ public class Etudiant {
 
    public void setGroupe(Groupe groupe) {
       this.groupe = groupe;
-      groupe.ajouterEtudiants(this);
+      if (getGroupe() == null){
+         groupe.ajouterEtudiants(this);
+      } 
+      else {
+         getGroupe().transfererEtudiants(groupe, this);
+      }
    }
 
    public String toString() {
